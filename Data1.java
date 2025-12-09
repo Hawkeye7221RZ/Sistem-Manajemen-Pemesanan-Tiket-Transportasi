bd4a70ab49e8f3f7f73a15e85f20a3cbde481fd3
Hawkeye7221RZ <164899280+Hawkeye7221RZ@users.noreply.github.com>
2025-12-09 17:29:52 +0700

    Buatt ini randu

--- Data1.java
+++ Data1.java
@@ -50,7 +50,7 @@ public class Data1 {
             System.out.print("Tanggal: ");
             String tanggal = input.next();
 
-             list.add(new Flight(id, pesawat, asal, tujuan, harga, tanggal));
+             list.add(new Flight(id, pesawat, asal, tujuan, harga, tanggall));
         }
             System.out.println("===== Data Penerbangan =====");
             int jumlah= input.nextInt();
